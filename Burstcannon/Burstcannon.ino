53ad2cf0c49416fb1dc84c40ceddb82f0c4ffd2c
Christian D. Madsen <sevenplagues@gmail.com>
2016-07-14 03:58:52 -0600

    devcode backup
    yes

--- Burstcannon/Burstcannon.ino
+++ Burstcannon/Burstcannon.ino
@@ -117,21 +117,22 @@ void parseKeystrokes() {
 void assignCommandAction(byte col, byte row) {
 	if (col <= 3) { // If this keystroke was in the left half of the button-grid...
 		if (row == 0) { // If this keystroke was on the first row...
-			CHAN |= 1 << (3 - col); // Toggle the bit in the channel-value that corresponds to the column's bitwise value
+			CHAN ^= 1 << (3 - col); // Toggle the bit in the channel-value that corresponds to the column's bitwise value
 			EEPROM.write(16, CHAN); // Write the new channel value to EEPROM memory, for storage across shutdowns
 		} else if (row == 3) { // Else, if this keystroke was on the fourth row...
-			VELOCITY |= 8 << (3 - col); // Toggle the bit in the velocity-value that corresponds to the column's bitwise value, left-shifted by 3 to (64-32-16-8)
+      VELOCITY = ((VELOCITY & 7) > 0) ? 0 : VELOCITY; // If the velocity is at a special value of 127 or 1, set it to 0 in preparation for a left-shifted bit
+			VELOCITY ^= 8 << (3 - col); // Toggle the bit in the velocity-value that corresponds to the column's bitwise value, left-shifted by 3 to (64-32-16-8)
+      VELOCITY |= (VELOCITY == 120) ? 7 : ((VELOCITY == 0) ? 1 : 0); // Set the velocity to 127 or 1 if it is filled or emptied
 		} else { // Else, if this keystroke was in one of the middle rows...
 			if ((row == 1) && (col == 0)) { // If this was the first button of the second row...
 				PITCH = max(0, min(127, PITCH + (((random(0, 1) * 2) - 1) * random(2, 5)))); // Change the global base-pitch by a random likely-to-be-consonant interval
 			} else { // Else, if this was any other button...
-				PITCH |= max(1, (2 - row) << 4) << (3 - col); // Modify the PITCH value by a binary value corresponding to a button in the middle two rows
+				PITCH ^= max(1, (2 - row) << 4) << (3 - col); // Modify the PITCH value by a binary value corresponding to a button in the middle two rows
 			}
 		}
 		ROWUPDATE |= 1 << row; // Flag the button-row's corresponding LED-row for a GUI update
 	} else { // Else, if the keystroke was in the right half of the button-grid...
-		byte rcol = col - 4; // Get an offset column-value for the right side of the grid
-		DENSITYINTERVAL[rcol] |= 1 << (rcol + ((row & 1) * 4)); // Flag the button's corresponding density-interval coordinates
+		DENSITYINTERVAL[row >> 1] |= 1 << ((col & 3) | ((row & 1) << 2)); // Flag the button's corresponding density-interval coordinates
 		ROWUPDATE |= B01110000; // Flag the burst-activity LED-rows for a GUI update
 	}
 }
@@ -139,8 +140,7 @@ void assignCommandAction(byte col, byte row) {
 // Interpret a key-release according to whatever command-mode is active
 void unassignCommandAction(byte col, byte row) {
 	if (col >= 4) { // If this key-release was in the right half of the button-grid...
-		byte rcol = col - 4; // Get an offset column-value for the right side of the grid
-		DENSITYINTERVAL[rcol] ^= 1 << (rcol + ((row & 1) * 4)); // Unflag the button's corresponding density-interval coordinates
+    DENSITYINTERVAL[row >> 1] ^= 1 << ((col & 3) | ((row & 1) << 2)); // Unflag the button's corresponding density-interval coordinates
     ROWUPDATE |= B01110000; // Flag the burst-activity LED-rows for a GUI update
 	}
 }
@@ -148,46 +148,40 @@ void unassignCommandAction(byte col, byte row) {
 // Play a note based on currently-active burst commands, if they correspond to the current global MIDI CLOCK tick
 void playBurst() {
 
+  byte mod[9]; // Density-values, arranged by their corresponding interval
+  byte locs[9]; // List of interval-locations in the "mod" array with any current amount of burst-density
+  byte pressed = 0; // Number of interval-types, represented by buttons, that are currently being held down
 
-  /* TODO rewrite this in a less-complex way
-
-  byte intervals[9] = {0, 0, 0, 0, 0, 0, 0, 0};
-  byte applicable = 0;
-  byte composite = 0;
-
-  for (byte i = 0; i < 16; i++) {
-    byte imask = i & 7;
-    byte igroup = i >> 3;
-    intervals[imask] |= (igroup + 1) * min(1, DISPLAYINTERVAL[igroup] & (1 << imask));
-  }
-
-  for (byte i = 0; i < 8; i++) {
-    if (intervals[i] < 3) {
-      if ((CURTICK % 6) > 0) {
-        intervals[i] = max(0, intervals[i] - 2);
-      } else if ((CURTICK % 3) > 0) {
-        intervals[i] = max(0, intervals[i] - 1);
-      }
+  for (byte i = 0; i < 8; i++) { // For every interval entry in the DENSITYINTERVAL array...
+    byte ibin = 1 << i; // Get the bitwise position of this entry
+    mod[i] = min(1, DENSITYINTERVAL[0] & ibin) << 2) * (1 - min(1, CURTICK % 6)); // If 1/6-density key is pressed, and the global tick matches, store the value
+    mod[i] |= (min(1, DENSITYINTERVAL[1] & ibin) << 1) * (1 - min(1, CURTICK % 3)); // If 1/3-density key is pressed, and the global tick matches, store the value
+    if (mod[i] == 6) { // If both the 1/6-density and 1/3-density keys are pressed for a given interval...
+      mod[i] = 1; // Set the interval's density to 1, aka "every tick"
     }
-    if ((intervals[i] & 3) > 0) {
-      applicable++;
+    if (mod[i] > 0) { // If the current interval has any density from button-presses...
+      locs[pressed] = i; // Store this interval in the "locations with any amount of density" array
+      pressed++; // Increase the "pressed" value to track the number of interval-types that are pressed
     }
   }
 
-  byte acount = 0;
-  for (byte i = 0; i < 8; i++) {
-    if (acount >= applicable) { break; }
-
+  byte combo = random(1, min(2, pressed)); // Number of intervals to apply to global PITCH for this burst press: either 1 or 2, based on keys currently held
+  byte outpitch = PITCH; // The output-pitch, to be modified by intervals in the user's keystrokes
+  byte interval = 0; // The current interval-type
+  byte lastused = 255; // The previous interval-type, for checking other interval-types against
+  byte applied = 0; // Number of intervals applied to the outgoing pitch
+  while (applied < combo) { // While fewer buttons have been applied than the number required for the interval-combination...
+    do { // Continually get an interval...
+      interval = mod[locs[random(1, pressed)]]; // ...From a random position among user-pressed interval types...
+    } while (interval == lastused); // ...Until that interval doesn't match the previous interval
+    lastused = interval; // This interval will be treated as the "previous" interval, on the next loop
+    outpitch = min(127, max(0, outpitch + interval)); // Apply the interval to the out-pitch, bounded to the minimum and maximum MIDI pitch values
+    applied++; // Increase the "applied" value to track the number of intervals that have been applied to the outgoing pitch
   }
-  */
 
+  sendNoteOn(CHAN, outpitch, VELOCITY, 1); // Send a note on the currently-active MIDI channel, with the outgoing pitch
 
-
-
-
-  sendNoteOn(CHAN, PITCH, VELOCITY, 1); // todo update this
-
-
+  ROWUPDATE |= B01110000; // Flag the burst-activity LED-rows for a GUI update
 
 }
 
@@ -304,6 +298,7 @@ void loop() {
         CURTICK = 0; // Set the current tick to the start-point
 			} else if (b == 248) { // If this is a TIMING CLOCK command...
 				Serial.write(b); // Send it onward to MIDI-OUT
+        CURTICK = (CURTICK + 1) % 96; // Increment the global tick-counter, bounded by the number of ticks in a beat
         incrementSustain(); // Increment the currently-sustained note
         playBurst(); // Play notes based on whatever burst-keys are being held
 			} else if (b == 247) { // If this is an END SYSEX MESSAGE command...
