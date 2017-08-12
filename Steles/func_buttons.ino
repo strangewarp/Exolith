8c486c3820f52eaf768d111b6f46f035bfe32ed7
Christian D. Madsen <sevenplagues@gmail.com>
2017-08-12 10:10:40 -0600

    devcode backup
    still updating savefile-adjacent things

--- Steles/func_buttons.ino
+++ Steles/func_buttons.ino
@@ -146,8 +146,6 @@ void parseRecPress(byte col, byte row) {
 		BPM = max(16, min(200, BPM ^ (128 >> (key % 8)))); // Change the BPM rate
 		updateFileByte(0, BPM); // Update the BPM-byte in the song's savefile
 		TO_UPDATE |= 252; // Flag the bottom 6 LED-rows for updating
-	} else if (ctrl == B00011100) { // If the SAVE command is held...
-		saveSong((PAGE * 24) + key); // Save the current tempfile into a savefile slot
 	} else if (ctrl == B00011010) { // If the CLOCK-MASTER command is held...
 		CLOCKMASTER ^= 1; // Toggle the CLOCK-MASTER value
 		ABSOLUTETIME = micros(); // Set the ABSOLUTETIME-tracking var to now
