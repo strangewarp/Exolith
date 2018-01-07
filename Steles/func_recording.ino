8728b802ae7961f5694aa01d3c28852c4f9ba738
Christian D. Madsen <sevenplagues@gmail.com>
2018-01-07 09:36:12 -0700

    debugging
    yes

--- Steles/func_recording.ino
+++ Steles/func_recording.ino
@@ -5,7 +5,7 @@ void recordToSeq(char offset, byte chan, byte b1, byte b2) {
 	byte buf[9] = {0, 0, 0, 0, 0, 0, 0, 0}; // SD-card read/write buffer
 
 	// Create a virtual tick-position that compensates for any given tick-offset
-	word tick = (POS[RECORDSEQ] + offset) % (word(STATS[RECORDSEQ] & 63) << 4);
+	word tick = word((int(POS[RECORDSEQ]) + offset) % (word(STATS[RECORDSEQ] & 63) << 4));
 
 	// Get the position of the first of this tick's bytes in the data-file
 	unsigned long tickstart = (49UL + (8192UL * RECORDSEQ)) + (tick << 3);
@@ -14,22 +14,22 @@ void recordToSeq(char offset, byte chan, byte b1, byte b2) {
 	file.seekSet(tickstart);
 	file.read(buf, 8);
 
-	byte toinsert = buf[3] ? 1 : 0; // Tracks which empty slot to insert the note into
-
 	// If none of the note-slots are empty, then shift their contents downward by one note, deleting the lowest item
 	if (buf[7]) {
 		file.seekSet(tickstart + 4);
 		file.write(buf, 4);
-		toinsert = 0; // Prepare to insert the new note into the first space in the tick
 	}
 
+	// Set the insert-point to the highest unfilled note in the tick,
+	// or to the first note in the tick, if the contents were shifted downward
+	file.seekSet(tickstart + (((!!buf[3]) ^ (!!buf[7])) << 2));
+
 	// Construct a virtual MIDI command, with an additional DURATION value, in the write buffer
-	buf[0] = (chan <= 15) ? DURATION : 0; // Only include the DURATION if this isn't a flagged special-command
+	buf[0] = (chan <= 15) * DURATION; // Only include the DURATION if this isn't a flagged special-command
 	buf[1] = chan;
 	buf[2] = b1;
 	buf[3] = b2;
 
-	file.seekSet(tickstart + (toinsert * 4)); // Set the insert-point to the highest unfilled note
 	file.write(buf, 4); // Write the note to the current tempdata
 
 	file.sync(); // Force this function's write processes to be committed to the savefile immediately
